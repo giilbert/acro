10de6c10d07e1f94c9e72c6e320d78d55981e47d
Gilbert Zhang <gilbertzhang1208@gmail.com>
2024-08-05 14:52:51 -0400

    change simple example

--- examples/simple/src/test.ts
+++ examples/simple/src/test.ts
@@ -9,11 +9,11 @@ class TestBehavior extends Behavior {
   }
 
   update() {
-    this.text.content = `is space pressed? ${Input.isKeyPressed("Space")}`;
+    this.text.content = `is a pressed? ${Input.isKeyPressed("KeyA")}`;
 
     this.transform.position.addAssign({ x: 0.00001, y: 0.00001, z: 0 });
     if (Input.isKeyPressed("Space")) {
-      this.transform.rotation.z += 0.0001;
+      this.transform.rotation.z -= 0.0001;
     }
     this.transform.scale = new Vec3(2, 2, 2);
   }
