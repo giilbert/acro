2f32fc0166f9af53de83b3af92ce612c4e2d862e
Gilbert Zhang <gilbertzhang1208@gmail.com>
2025-01-22 20:23:56 -0500

    me when i replace bad code with even worse code

--- crates/acro_scripting/src/runtime.rs
+++ crates/acro_scripting/src/runtime.rs
@@ -1,19 +1,255 @@
-use std::{borrow::Cow, cell::RefCell, collections::HashMap, rc::Rc, time::SystemTime};
+use std::{cell::RefCell, collections::HashMap, rc::Rc, time::SystemTime};
 
 use acro_assets::Assets;
 use acro_ecs::{Changed, ComponentId, EntityId, Query, Res, ResMut, SystemRunContext, Tick, World};
 use acro_reflect::Reflect;
 
-cfg_if::cfg_if! {
-    if #[cfg(target_arch = "wasm32")] {
-        use js_sys::Function;
-    } else {
-        use deno_core::url::Url;
-        use rustyscript::{
-            deno_core, json_args, module_loader::ImportProvider, Module, ModuleHandle,
-            Runtime as JsRuntime, RuntimeOptions, Undefined,
-        };
+pub trait Platform {
+    fn new() -> Self;
+
+    fn init_source_file(
+        &mut self,
+        component_vtables: &mut ComponentVTables,
+        source_file: &SourceFile,
+    ) -> eyre::Result<()>;
+    fn init_behavior(
+        &mut self,
+        id: u32,
+        attached_to: EntityId,
+        source_file: &SourceFile,
+        behavior: &mut Behavior,
+    ) -> eyre::Result<()>;
+    fn update(&mut self, last_update: SystemTime, tick: Tick) -> eyre::Result<SystemTime>;
+    fn late_init(&self) -> eyre::Result<()>;
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+mod runtime_impl {
+    use std::borrow::Cow;
+
+    use super::*;
+    use deno_core::url::Url;
+    use rustyscript::{
+        deno_core, json_args, module_loader::ImportProvider, Module, ModuleHandle,
+        Runtime as JsRuntime, RuntimeOptions, Undefined,
+    };
+
+    pub struct NativePlatform {
+        ops_dec: Option<Vec<deno_core::OpDecl>>,
+        inner: Option<JsRuntime>,
+        init_module_handle: Option<ModuleHandle>,
     }
+
+    impl NativePlatform {
+        pub fn inner(&self) -> &JsRuntime {
+            self.inner
+                .as_ref()
+                .expect("js runtime has not been initialized")
+        }
+
+        pub fn inner_mut(&mut self) -> &mut JsRuntime {
+            self.inner
+                .as_mut()
+                .expect("js runtime has not been initialized")
+        }
+    }
+
+    impl super::Platform for NativePlatform {
+        fn new() -> Self {
+            Self {
+                ops_dec: Some(Vec::new()),
+                inner: None,
+                init_module_handle: None,
+            }
+        }
+
+        fn init_source_file(
+            &mut self,
+            component_vtables: &mut ComponentVTables,
+            source_file: &SourceFile,
+        ) -> eyre::Result<()> {
+            if component_vtables.is_some() {
+                let component_vtables = component_vtables.take().expect("component vtables taken");
+                self.inner_mut()
+                    .deno_runtime()
+                    .op_state()
+                    .borrow_mut()
+                    .put(component_vtables);
+            }
+
+            tracing::info!("initializing source file: {:?}", source_file.config.name);
+
+            // TODO: cleanup module after it has been unloaded
+            let module_handle = self.inner_mut().load_module(&Module::new(
+                &format!("./lib/{}.ts", source_file.config.name),
+                &source_file.code,
+            ))?;
+            self.inner_mut()
+                .call_entrypoint::<Undefined>(&module_handle, json_args!())?;
+
+            Ok(())
+        }
+
+        fn init_behavior(
+            &mut self,
+            id: u32,
+            attached_to: EntityId,
+            source_file: &SourceFile,
+            behavior: &mut Behavior,
+        ) -> eyre::Result<()> {
+            behavior.data = Some(BehaviorData { id });
+
+            let module_handle = self.init_module_handle.as_ref().map(|h| h.clone());
+            self.inner_mut().call_function(
+                module_handle.as_ref(),
+                "createBehavior",
+                json_args!(
+                    attached_to.generation,
+                    attached_to.index,
+                    id,
+                    source_file.config.name.as_str()
+                ),
+            )?;
+
+            Ok(())
+        }
+
+        fn update(&mut self, last_update: SystemTime, tick: Tick) -> eyre::Result<SystemTime> {
+            self.inner_mut()
+                .deno_runtime()
+                .op_state()
+                .borrow_mut()
+                .put(tick);
+
+            let delta_time = last_update.elapsed()?.as_secs_f64();
+
+            let module_handle = self.init_module_handle.as_ref().map(|h| h.clone());
+            self.inner_mut().call_function(
+                module_handle.as_ref(),
+                "update",
+                json_args!(delta_time),
+            )?;
+
+            Ok(SystemTime::now())
+        }
+
+        fn late_init(&self, world_handle: Rc<RefCell<World>>) -> eyre::Result<()> {
+            if self.inner.is_some() {
+                return Ok(());
+            }
+
+            {
+                let mut registered_ops = self.ops_dec.take().expect("ops already taken");
+                use crate::platform::deno_ops::{
+                    op_call_function, op_get_property_number, op_get_property_string,
+                    op_set_property_number, op_set_property_string,
+                };
+                registered_ops.push(op_get_property_string());
+                registered_ops.push(op_set_property_string());
+                registered_ops.push(op_get_property_number());
+                registered_ops.push(op_set_property_number());
+                registered_ops.push(op_call_function());
+
+                let ext = deno_core::Extension {
+                    name: "reflect",
+                    ops: Cow::Owned(registered_ops),
+                    ..Default::default()
+                };
+
+                let mut runtime = JsRuntime::new(RuntimeOptions {
+                    extensions: vec![ext],
+                    default_entrypoint: Some("init".to_string()),
+                    import_provider: Some(Box::new(AcroLibImportProvider)),
+                    ..Default::default()
+                })?;
+
+                {
+                    let op_state = runtime.deno_runtime().op_state();
+                    let mut op_state = op_state.borrow_mut();
+
+                    op_state.put(world_handle.clone());
+                    op_state.put(Tick::new(0));
+                }
+
+                let init_module = Module::load("lib/core/init.ts")?;
+                let init_module_handle = runtime
+                    .load_module(&init_module)
+                    .expect("error loading init module");
+
+                runtime.call_function(Some(&init_module_handle), "init", json_args!())?;
+
+                info!(
+                    "registered {} component(s)",
+                    self.name_to_component_id.len()
+                );
+
+                runtime.call_function(
+                    Some(&init_module_handle),
+                    "registerComponents",
+                    json_args!(name_to_component_id),
+                )?;
+
+                runtime.inner = Some(runtime);
+                self.init_module_handle = Some(init_module_handle);
+            }
+
+            Ok(())
+        }
+    }
+
+    #[derive(Default)]
+    pub struct AcroLibImportProvider;
+
+    impl ImportProvider for AcroLibImportProvider {
+        fn resolve(
+            &mut self,
+            specifier: &deno_core::ModuleSpecifier,
+            _referrer: &str,
+            _kind: deno_core::ResolutionKind,
+        ) -> Option<Result<deno_core::ModuleSpecifier, deno_core::anyhow::Error>> {
+            match specifier.scheme() {
+                "jsr" if specifier.path().starts_with("@acro/") => {
+                    let mut cwd = std::env::current_dir().expect("failed to get current directory");
+                    cwd.push(&format!(
+                        "lib/{}/mod.ts",
+                        specifier.path().replace("@acro/", "")
+                    ));
+                    cwd.to_str()
+                        .map(|path| Ok(Url::parse(&format!("file://{}", path)).unwrap()))
+                }
+                _ => None,
+            }
+        }
+    }
+
+    pub use NativePlatform as Platform;
+
+    use crate::SourceFile;
+
+    use super::ComponentVTables;
+}
+
+#[cfg(target_arch = "wasm32")]
+mod runtime_impl {
+    use js_sys::Function;
+
+    pub struct WasmPlatform {}
+
+    impl super::Platform for WasmPlatform {
+        fn new() -> Self {
+            todo!();
+        }
+
+        fn init_source_file(
+            &mut self,
+            component_vtables: &mut super::ComponentVTables,
+            source_file: &crate::SourceFile,
+        ) -> eyre::Result<()> {
+            todo!();
+        }
+    }
+
+    pub use WasmPlatform as Platform;
 }
 
 use tracing::info;
@@ -24,16 +260,15 @@ use crate::{
     EventListenerStore,
 };
 
+type ComponentVTables = Option<HashMap<ComponentId, *const ()>>;
+
 pub struct ScriptingRuntime {
     last_update: SystemTime,
     world_handle: Rc<RefCell<World>>,
     behavior_id: u32,
     name_to_component_id: HashMap<String, ComponentId>,
-
-    decl: Option<Vec<deno_core::OpDecl>>,
-    inner: Option<JsRuntime>,
-    component_vtables: Option<HashMap<ComponentId, *const ()>>,
-    init_module_handle: Option<ModuleHandle>,
+    component_vtables: ComponentVTables,
+    platform: runtime_impl::Platform,
 }
 
 impl std::fmt::Debug for ScriptingRuntime {
@@ -51,23 +286,10 @@ impl ScriptingRuntime {
             behavior_id: 0,
             world_handle,
             name_to_component_id: HashMap::new(),
-            decl: Some(vec![]),
-            inner: None,
             component_vtables: Some(HashMap::new()),
-            init_module_handle: None,
+
+            platform: runtime_impl::Platform::new(),
         }
-    }
-
-    pub fn inner(&self) -> &JsRuntime {
-        self.inner
-            .as_ref()
-            .expect("js runtime has not been initialized")
-    }
-
-    pub fn inner_mut(&mut self) -> &mut JsRuntime {
-        self.inner
-            .as_mut()
-            .expect("js runtime has not been initialized")
     }
 
     pub fn register_component<T: Reflect + 'static>(&mut self, name: &str) {
@@ -89,29 +311,8 @@ impl ScriptingRuntime {
     }
 
     pub fn init_source_file(&mut self, source_file: &SourceFile) -> eyre::Result<()> {
-        if self.component_vtables.is_some() {
-            let component_vtables = self
-                .component_vtables
-                .take()
-                .expect("component vtables taken");
-            self.inner_mut()
-                .deno_runtime()
-                .op_state()
-                .borrow_mut()
-                .put(component_vtables);
-        }
-
-        info!("initializing source file: {:?}", source_file.config.name);
-
-        // TODO: cleanup module after it has been unloaded
-        let module_handle = self.inner_mut().load_module(&Module::new(
-            &format!("./lib/{}.ts", source_file.config.name),
-            &source_file.code,
-        ))?;
-        self.inner_mut()
-            .call_entrypoint::<Undefined>(&module_handle, json_args!())?;
-
-        Ok(())
+        self.platform
+            .init_source_file(&mut self.component_vtables, source_file)
     }
 
     pub fn init_behavior(
@@ -122,143 +323,35 @@ impl ScriptingRuntime {
     ) -> eyre::Result<()> {
         let id = self.behavior_id;
         self.behavior_id += 1;
-        behavior.data = Some(BehaviorData { id });
 
-        let module_handle = self.init_module_handle.as_ref().map(|h| h.clone());
-        self.inner_mut().call_function(
-            module_handle.as_ref(),
-            "createBehavior",
-            json_args!(
-                attached_to.generation,
-                attached_to.index,
-                id,
-                source_file.config.name.as_str()
-            ),
-        )?;
-
-        Ok(())
+        self.platform
+            .init_behavior(id, attached_to, source_file, behavior)
     }
 
     pub fn update(&mut self, tick: Tick) -> eyre::Result<()> {
-        self.inner_mut()
-            .deno_runtime()
-            .op_state()
-            .borrow_mut()
-            .put(tick);
-
-        let delta_time = self.last_update.elapsed()?.as_secs_f64();
-
-        let module_handle = self.init_module_handle.as_ref().map(|h| h.clone());
-        self.inner_mut()
-            .call_function(module_handle.as_ref(), "update", json_args!(delta_time))?;
-
-        self.last_update = SystemTime::now();
-
+        self.last_update = self.platform.update(self.last_update, tick)?;
         Ok(())
     }
 
-    pub fn add_op(&mut self, decl: deno_core::OpDecl) -> &mut Self {
-        self.decl
-            .as_mut()
-            .expect("ops already initialized")
-            .push(decl);
-        self
-    }
+    pub fn late_init(&self) {}
 
-    pub fn take_ops(&mut self) -> Vec<deno_core::OpDecl> {
-        self.decl.take().expect("ops already initialized")
-    }
-}
+    // pub fn add_op(&mut self, decl: deno_core::OpDecl) -> &mut Self {
+    //     self.decl
+    //         .as_mut()
+    //         .expect("ops already initialized")
+    //         .push(decl);
+    //     self
+    // }
 
-#[derive(Default)]
-pub struct AcroLibImportProvider;
-
-impl ImportProvider for AcroLibImportProvider {
-    fn resolve(
-        &mut self,
-        specifier: &deno_core::ModuleSpecifier,
-        _referrer: &str,
-        _kind: deno_core::ResolutionKind,
-    ) -> Option<Result<deno_core::ModuleSpecifier, deno_core::anyhow::Error>> {
-        match specifier.scheme() {
-            "jsr" if specifier.path().starts_with("@acro/") => {
-                let mut cwd = std::env::current_dir().expect("failed to get current directory");
-                cwd.push(&format!(
-                    "lib/{}/mod.ts",
-                    specifier.path().replace("@acro/", "")
-                ));
-                cwd.to_str()
-                    .map(|path| Ok(Url::parse(&format!("file://{}", path)).unwrap()))
-            }
-            _ => None,
-        }
-    }
+    // pub fn take_ops(&mut self) -> Vec<deno_core::OpDecl> {
+    //     self.decl.take().expect("ops already initialized")
+    // }
 }
 
 pub fn init_scripting_runtime(
     _ctx: SystemRunContext,
     mut runtime: ResMut<ScriptingRuntime>,
 ) -> eyre::Result<()> {
-    if runtime.inner.is_some() {
-        return Ok(());
-    }
-
-    #[cfg(not(target_arch = "wasm32"))]
-    {
-        let mut registered_ops = runtime.take_ops();
-        use crate::platform::deno_ops::{
-            op_call_function, op_get_property_number, op_get_property_string,
-            op_set_property_number, op_set_property_string,
-        };
-        registered_ops.push(op_get_property_string());
-        registered_ops.push(op_set_property_string());
-        registered_ops.push(op_get_property_number());
-        registered_ops.push(op_set_property_number());
-        registered_ops.push(op_call_function());
-
-        let ext = deno_core::Extension {
-            name: "reflect",
-            ops: Cow::Owned(registered_ops),
-            ..Default::default()
-        };
-
-        let mut inner_runtime = JsRuntime::new(RuntimeOptions {
-            extensions: vec![ext],
-            default_entrypoint: Some("init".to_string()),
-            import_provider: Some(Box::new(AcroLibImportProvider)),
-            ..Default::default()
-        })?;
-
-        {
-            let op_state = inner_runtime.deno_runtime().op_state();
-            let mut op_state = op_state.borrow_mut();
-
-            op_state.put(runtime.world_handle.clone());
-            op_state.put(Tick::new(0));
-        }
-
-        let init_module = Module::load("lib/core/init.ts")?;
-        let init_module_handle = inner_runtime
-            .load_module(&init_module)
-            .expect("error loading init module");
-
-        inner_runtime.call_function(Some(&init_module_handle), "init", json_args!())?;
-
-        info!(
-            "registered {} component(s)",
-            runtime.name_to_component_id.len()
-        );
-
-        inner_runtime.call_function(
-            Some(&init_module_handle),
-            "registerComponents",
-            json_args!(runtime.name_to_component_id),
-        )?;
-
-        runtime.inner = Some(inner_runtime);
-        runtime.init_module_handle = Some(init_module_handle);
-    }
-
     Ok(())
 }
 
