c56829ee5eb24eb3b8ad18f5dddf0243c4fb5d72
gilbert <40220369+giilbert@users.noreply.github.com>
2025-02-04 21:25:35 -0500

    oops

--- examples/simple/build/entry.ts
+++
@@ -1,9 +0,0 @@
-import { init } from "jsr:@acro/core";
-init();
-// deno-lint-ignore-file no-explicit-any
-import * as file_0 from "../src/test.ts";
-(file_0 as any).init?.();
-import * as file_1 from "../build/entry.ts";
-(file_1 as any).init?.();
-import * as file_2 from "../src/fly-camera.ts";
-(file_2 as any).init?.();
